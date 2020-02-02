017ad79cbc9924c6cdcb3f4c88ed155d522c4579
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-02 22:32:23 +0100

    Add echos

---
+++ cooar/utilities/echo.py
@@ -0,0 +1,45 @@
+import click
+
+
+def message(msg: str, color: str = "bright_white", bold=False):
+    message = click.style(msg, fg=color, bold=bold)
+    click.echo(message)
+
+
+def debug_msg(msg: str):
+    debug_color = "bright_black"
+    ctx = click.get_current_context()
+    if ctx.obj.get("DEBUG"):
+        _prefixed_message("DEBUG", msg, debug_color)
+
+
+def warning_msg(msg: str):
+    warning_color = "bright_yellow"
+    _prefixed_message("WARNING", msg, warning_color)
+
+
+def error_msg(msg: str):
+    error_color = "bright_red"
+    _prefixed_message("ERROR", msg, error_color)
+
+
+def _prefixed_message(prefix: str, msg: str, color: str, err: bool = False):
+    prefix_msg = click.style(prefix + ": ", fg=color, bold=True)
+    message = click.style(msg, fg=color, bold=False)
+    if err is True:
+        click.echo(prefix_msg + message, err=msg)
+    else:
+        click.echo(prefix_msg + message)
+
+
+def key_value_message(key: str, value: str, key_color: str, value_color: str):
+    key_msg = click.style(key + ": ", fg=key_color)
+    value_msg = click.style(value, fg=value_color)
+    click.echo(key_msg + value_msg)
+
+
+def bool_style(value: bool, true_color: str = "green", false_color: str = "red"):
+    if value is True:
+        return click.style("True", fg=true_color)
+    else:
+        return click.style("False", fg=false_color)
---
+++ tests/cooar/utilities/test_echo.py
@@ -0,0 +1,84 @@
+# pylint: disable=no-member
+import pytest
+from unittest import mock
+from cooar.utilities import echo
+import click
+
+
+class TestEcho:
+    def reset(self, click):
+        click.echo.reset_mock()
+        click.style.reset_mock()
+
+    @mock.patch("click.style", mock.MagicMock())
+    @mock.patch("click.echo", mock.MagicMock())
+    def test_message(self):
+        self.reset(click)
+        echo.message("TEST", color="green", bold=False)
+        click.style.assert_called_once_with("TEST", fg="green", bold=False)
+        assert click.echo.called
+
+    @mock.patch("click.style", mock.MagicMock())
+    @mock.patch("click.echo", mock.MagicMock())
+    @pytest.mark.parametrize("is_err", [True, False])
+    def test__prefixed_message(self, is_err):
+        self.reset(click)
+        echo._prefixed_message("LULULUL", "message", "green", is_err)
+        click.style.assert_has_calls(
+            [
+                mock.call("LULULUL: ", fg="green", bold=True),
+                mock.call("message", fg="green", bold=False),
+            ]
+        )
+        if is_err is True:
+            click.echo.assert_called_once_with(mock.ANY, err="message")
+
+        else:
+            click.echo.assert_called_once_with(mock.ANY)
+
+    @mock.patch("click.style", mock.MagicMock())
+    @mock.patch("click.echo", mock.MagicMock())
+    def test_key_value_message(self):
+        self.reset(click)
+        echo.key_value_message("key", "value", "green", "green")
+        click.style.assert_has_calls(
+            [mock.call("key: ", fg="green"), mock.call("value", fg="green"),]
+        )
+        click.echo.assert_called_once_with(mock.ANY)
+
+    @mock.patch("click.style", mock.MagicMock())
+    @pytest.mark.parametrize("value", [True, False])
+    def test_bool_style(self, value):
+        click.style.reset_mock()
+        echo.bool_style(value)
+        if value is True:
+            click.style.assert_called_once_with("True", fg="green")
+        else:
+            click.style.assert_called_once_with("False", fg="red")
+
+    @mock.patch("click.get_current_context", mock.MagicMock())
+    @mock.patch("cooar.utilities.echo._prefixed_message", mock.MagicMock())
+    @pytest.mark.parametrize("debug", [True, False])
+    def test_debug_msg(self,debug):
+        echo._prefixed_message.reset_mock()
+        class FakeContext():
+                obj = {}
+                if debug is True:
+                    obj['DEBUG'] = True
+        click.get_current_context.return_value = FakeContext()
+
+        echo.debug_msg('Test')
+        if debug is True:
+            echo._prefixed_message.assert_called_once_with("DEBUG", 'Test', "bright_black")
+        else:
+            assert not echo._prefixed_message.called
+
+    @mock.patch("cooar.utilities.echo._prefixed_message", mock.MagicMock())
+    def test_warning_msg(self):
+        echo.warning_msg('Test')
+        echo._prefixed_message.assert_called_once_with("WARNING", 'Test', "bright_yellow")
+
+    @mock.patch("cooar.utilities.echo._prefixed_message", mock.MagicMock())
+    def test_error_msg(self):
+        echo.error_msg('Test')
+        echo._prefixed_message.assert_called_once_with("ERROR", 'Test', "bright_red")
