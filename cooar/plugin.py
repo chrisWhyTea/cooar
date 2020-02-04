72b6d51daaf763cbb3587154929fe55a4dadf40b
Christopher Schmitt <52947016+chrisWhyTea@users.noreply.github.com>
2020-02-04 21:24:00 +0100

    Adjust and test PluginABC

--- cooar/plugin.py
+++ cooar/plugin.py
@@ -1,15 +1,64 @@
 from abc import ABC, abstractmethod
+from typing import Dict, List, Tuple
+
+from cooar.utilities.types import AuthType, MediaType
 
 
-class CooarPlugin(ABC):
+class CooarPluginInfoABC(ABC):
+    """
+    All the info properties, so they dont bother while working with the acctual plugin class
+    """
+
+    @property
     @abstractmethod
-    def prepare(self):
+    def name(self) -> str:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def url(self) -> str:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def author(self) -> str:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def supported_mediatypes(self) -> Tuple[MediaType, ...]:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def supported_authtypes(self) -> Tuple[AuthType, ...]:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def supported_qualities(self) -> Dict:
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def template_strings(self) -> Tuple[str, ...]:
+        raise NotImplementedError()
+
+
+class CooarPlugin(CooarPluginInfoABC, ABC):
+    @abstractmethod
+    def prepare(self, **kwargs):
         raise NotImplementedError()
 
     @abstractmethod
-    def collect(self):
+    def collect(self, part_id=None, **kwargs) -> List:
         raise NotImplementedError()
 
     @abstractmethod
-    def download(self):
+    def download(self, file, **kwargs):
         raise NotImplementedError()
---
+++ tests/cooar/test_plugin.py
@@ -0,0 +1,27 @@
+from cooar.plugin import CooarPlugin
+from cooar.utilities.types import AuthType, MediaType
+
+
+class TestCooarPlugin:
+    def test_instantiate_cooar_plugin(self):
+        class Plugin(CooarPlugin):
+            name = "plugin"
+            description = "a test plugin"
+            url = ""
+            author = "Christopher Schmitt <cooar@chris.yt>"
+            supported_mediatypes = (MediaType.VIDEO,)
+            supported_authtypes = (AuthType.NO_AUTH,)
+            supported_qualities = {MediaType.VIDEO: ("SD", "HD", "FullHD")}
+            template_strings = ("name", "lenght", "site")
+
+            def prepare(self, **kwargs):
+                raise NotImplementedError()
+
+            def collect(self, part_id=None, **kwargs):
+                raise NotImplementedError()
+
+            def download(self, file, **kwargs):
+                raise NotImplementedError()
+
+        plugin = Plugin()
+        assert plugin.supported_qualities[MediaType.VIDEO] == ("SD", "HD", "FullHD")
